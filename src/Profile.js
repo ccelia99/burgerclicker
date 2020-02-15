ad1a8fb037bb716c4b6d06f6f6dc5fd65edf4f04
Serja Aaltonen <serja.aaltonen@gmail.com>
2020-02-15 13:54:58 +0200

    Routing added

---
+++ src/Profile.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+
+class Pfofile extends React.Component {
+
+    render(){
+        return(
+            <>
+                <div className='header'>
+                    <h1>Profile</h1>
+                </div>
+                <div className="content">
+                    <p>No profile</p>
+                </div>
+            </>
+        );
+    }
+}
+export default Pfofile;
+  
